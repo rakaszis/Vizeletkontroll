f59efa563d08a0141625a35975e0377b1449bb38
rakaszis <istvanrakasz@yahoo.com>
2017-06-16 16:43:08 +0200

    Add files via upload

---
+++ js/vagabondmuvekbranding.js
@@ -0,0 +1,23 @@
+(function($) {          
+    $(document).ready(function(){                    
+        $(window).scroll(function(){                          
+            if ($(this).scrollTop() < 2000) {
+                $('#vagabond-brand').fadeOut(1500);
+            } else {
+                $('#vagabond-brand').fadeIn(1500);
+            }
+        });
+    });
+})(jQuery);
+
+(function($) {          
+    $(document).ready(function(){                    
+        $(window).scroll(function(){                          
+            if ($(this).scrollTop() > 50) {
+                $('.hidingmenu').fadeIn(3500);
+            } else {
+                $('.hidingmenu').fadeOut(50);
+            }
+        });
+    });
+})(jQuery);
